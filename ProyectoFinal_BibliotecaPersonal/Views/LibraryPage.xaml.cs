c057372d0bf230d33147263b639a03bacb1542cd
JavierTLoina <tejedajavier063@gmail.com>
2026-04-20 18:08:30 -0400

    Estilos y apaiencia visual

--- ProyectoFinal_BibliotecaPersonal/Views/LibraryPage.xaml.cs
+++ ProyectoFinal_BibliotecaPersonal/Views/LibraryPage.xaml.cs
@@ -1,3 +1,5 @@
+using ProyectoFinal_BibliotecaPersonal.Models;
+
 namespace ProyectoFinal_BibliotecaPersonal.Views;
 
 public partial class LibraryPage : ContentPage
@@ -5,5 +7,30 @@ public partial class LibraryPage : ContentPage
     public LibraryPage()
     {
         InitializeComponent();
+
+        listaLibros.ItemsSource = new List<Book>
+        {
+            new Book
+            {
+                Title = "El Psicoanalista",
+                Author = "John Katzenbach",
+                Genre = "Thriller",
+                Year = 2002,
+                IsRead = true
+            },
+            new Book
+            {
+                Title = "Cien años de soledad",
+                Author = "Gabriel García Márquez",
+                Genre = "Realismo Mágico",
+                Year = 1967,
+                IsRead = false
+            }
+        };
+    }
+
+    private async void OnAddBookClicked(object sender, EventArgs e)
+    {
+        await Shell.Current.GoToAsync(nameof(AddBookPage));
     }
 }
